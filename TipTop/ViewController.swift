09afcb35f5a24231c8838b50c206e2cd58399780
noor <muhammadnoor007@gmail.com>
2016-12-14 16:55:36 +0700

    add some fuction

--- TipTop/ViewController.swift
+++ TipTop/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var helloText: UILabel!
+    @IBAction func changeColor(_ sender: Any) {
+        
+        helloText.text = "Hello, Universe!"
+        helloText.backgroundColor = UIColor.brown
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
